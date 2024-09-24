acc965a437804a8b6b8132553dd6019e2821e510
Rasmus Bäckström <rasmusbaeckstrom@gmail.com>
2024-09-24 16:24:34 +0200

    All classes updated

--- src/main/java/org/example/FridayDiscount.java
+++ src/main/java/org/example/FridayDiscount.java
@@ -1,4 +1,26 @@
 package org.example;
 
-public class FridayDiscount extends Discount{
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+
+public class FridayDiscount extends BaseDiscount {
+
+    public FridayDiscount(Discount nextDiscount) {
+        super(nextDiscount);
+    }
+
+    @Override
+    protected boolean isApplicable(Product product) {
+        return LocalDate.now().getDayOfWeek() == DayOfWeek.FRIDAY;
+    }
+
+    @Override
+    protected double calculateDiscount(Product product) {
+        return product.price() * 0.10 * product.quantity();
+    }
+
+    @Override
+    protected String getSpecificDescription() {
+        return "10% off on Fridays";
+    }
 }
--- src/main/java/org/example/MilkDiscount.java
+++ src/main/java/org/example/MilkDiscount.java
@@ -1,4 +1,23 @@
 package org.example;
 
-public class MilkDiscount {
+public class MilkDiscount extends BaseDiscount {
+
+    public MilkDiscount(Discount nextDiscount) {
+        super(nextDiscount);
+    }
+
+    @Override
+    protected boolean isApplicable(Product product) {
+        return product.name().equalsIgnoreCase("milk");
+    }
+
+    @Override
+    protected double calculateDiscount(Product product) {
+        return product.price() * 0.05 * product.quantity();
+    }
+
+    @Override
+    protected String getSpecificDescription() {
+        return "5% off on milk";
+    }
 }
